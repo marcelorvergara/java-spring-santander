663eb59d2eca7437bfc2f6de637a23d4212e94a9
Marcelo Vergara <marcelorv@gmail.com>
2023-10-12 11:16:33 -0300

    first commit

---
+++ src/main/java/org/dio/SantanderFullStackDevApplication.java
@@ -0,0 +1,13 @@
+package org.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderFullStackDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderFullStackDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dio/SantanderFullStackDevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderFullStackDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
