363c93521856cb609d5f700ac66850aeeda761f4
Marcelo Vergara <marcelorv@gmail.com>
2023-10-12 12:31:25 -0300

    ORM structure for Jpa

--- src/main/java/org/dio/domain/model/User.java
+++ src/main/java/org/dio/domain/model/User.java
@@ -1,13 +1,20 @@
 package org.dio.domain.model;
 
-import java.util.List;
+import jakarta.persistence.*;
 
+import java.util.List;
+@Entity(name = "tb_user")
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToOne(cascade = CascadeType.ALL)
     private Account account;
+    @OneToOne(cascade = CascadeType.ALL)
     private Card card;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Feature> features;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> news;
 
     public Long getId() {
